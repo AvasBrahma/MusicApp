b9f19f46b8b680c40499314548724074ccb76373
AvasBrahma <avasbrahma@gmail.com>
2023-04-07 13:25:19 +0530

    added ejs view engine and load layouts and home controller

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express=require('express');
 const app=express();
 const port=8000;
+const expressLayouts=require('express-ejs-layouts');
+
+app.use(expressLayouts);
+
+//setting the layout extract styles & scripts so that script & style tag will move to specific place
+app.set('layout extractStyles', true);
+app.set('layouts extractScripts', true);
+
+app.use(express.static('./assets'))
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+
+app.use('/', require('./routes'));
+
+
+
+
 
 app.listen(port, function(err){
     if(err){
